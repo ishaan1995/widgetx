d8f0713b8fb30e2b562ed393b70a52d046bbc22e
ishaan1995 <ishaankakkar95@gmail.com>
2020-05-12 18:26:29 +0530

    [nightly] added first draft of widgetx extensions

--- lib/widgetx.dart
+++ lib/widgetx.dart
@@ -1,7 +1,38 @@
 library widgetx;
 
+import 'package:flutter/material.dart';
+
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
+}
+
+//// Singleton class of WidgetXHelper
+/// To enable / disable extension, provide global overrides
+class WidgetXHelper {
+
+  //// Default margin presets if any
+  ///
+  EdgeInsets defaultMargin() {
+    return EdgeInsets.all(16.0);
+  }
+}
+
+WidgetXHelper helperx = WidgetXHelper();
+
+//// WidgetX extensions to power our flutter widgets
+/// Highly experimental. API not stable yet.
+extension WidgetX on Widget {
+  Widget container(EdgeInsets margin) {
+    return Container(child: this, margin: margin ?? helperx.defaultMargin());
+  }
+  
+  Widget scaffold() {
+    return Scaffold(body: this);
+  }
+  
+  Widget center() {
+    return Center(child: this);
+  }
 }
